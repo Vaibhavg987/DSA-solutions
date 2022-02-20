262bf61cacc2784270022a888f36fc3e659cb310
Vaibhav <78644114+Vaibhavg987@users.noreply.github.com>
2022-02-20 18:07:40 +0530

    Time: 10 ms (91.32%), Space: 42.8 MB (51.58%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,50 +1,33 @@
 class Solution {
-    public static List<List<Integer>> sum4(int[] nums,int target){
-        List<List<Integer>> l = new ArrayList<>();
+    public static List<List<Integer>> fourSum(int[] nums,int target){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         int n=nums.length;
-		///The three loop costs O(n^3) and binary search costs O(logn)
-        for(int i=0;i<n-3;i++){
-            int a = nums[i];
-            for(int j=i+1;j<n-2;j++){
-                int b = nums[j];
-                for(int k=j+1;k<n-1;k++){
-                    List<Integer> l1 = new ArrayList<Integer>();
-                    int c= nums[k];
-                    int x=target-(a+b+c);
-                    if(binarySearch(nums,k+1,n-1,x)){
-                        l1.add(a);
-                        l1.add(b);
-                        l1.add(c);
-                        l1.add(x);
-						///checking whether the list contains Duplicates
-                        if(l.contains(l1)) continue;
-                        else l.add(new ArrayList<Integer>(l1));;
-                        // System.out.println(a+" "+b+" "+c+" "+x);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int front = j+1;
+                int back = n-1;
+                int target2 = target - nums[i] - nums[j];
+                while(front < back){
+                    int sum = nums[front] + nums[back];
+                    if(sum < target2) front++;
+                    else if(sum > target2) back--;
+                    else{
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[front]);
+                        list.add(nums[back]);
+                        ans.add(list);
+                        
+                        while(front < back && nums[front] == list.get(2)) ++front;
+                        while(front < back && nums[back] == list.get(3)) --back;
                     }
                 }
+                while(j+1 < n && nums[j+1] == nums[j]) ++j;
             }
+            while(i+1 < n && nums[i+1] == nums[i]) ++i;
         }
-        return l;
-    }
-    public static boolean binarySearch(int[] nums,int low,int high,int Element){
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==Element){
-                return true;
-            } 
-            else if(Element>nums[mid]){
-                low=mid+1;
-            }
-            else if(Element<nums[mid]){
-                high=mid-1;
-            }
-        }
-        // System.out.println(Element+" "+nums[mid]);
-        return false;
-    }
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-		///sorting costs o(nlogn)
-        return sum4(nums,target);
+        return ans;
     }
 }
