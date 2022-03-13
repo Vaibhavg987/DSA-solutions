fc4fb0aaac4b05d975628eaf269f4e84e69903d8
Vaibhav <78644114+Vaibhavg987@users.noreply.github.com>
2022-03-13 17:52:34 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (62.12%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,21 +2,20 @@ class Solution {
    public int uniquePathsWithObstacles(int[][] arr) {
         int m = arr.length ;
         int n = arr[0].length;
-    
-        return solve(m-1 , n - 1 , arr, new int[m][n]);
-    }
-    int solve(int m, int n, int[][] arr, int[][] dp){
-        if(m>=0 && n>=0 && arr[m][n]==1)return 0;
-      
-        if(m<0 || n<0)return 0;
-      
-        if(m==0 && n==0)return 1;
-    
-        if(dp[m][n]>0) return dp[m][n];
-
-        int up = solve(m-1, n, arr, dp);
-        int left = solve(m, n-1, arr, dp);
+        int[][] dp = new int[m][n];
         
-        return dp[m][n] = left+up;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==1) dp[i][j] = 0;
+                else if(i==0&&j==0) dp[i][j] = 1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+       return dp[m-1][n-1];
     }
 }
