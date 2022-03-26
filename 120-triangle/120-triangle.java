78db4795f342b86ee8bdd04959adc5ef5ec1f69f
Vaibhav <78644114+Vaibhavg987@users.noreply.github.com>
2022-03-26 11:36:11 +0530

    Time: 5 ms (50.52%), Space: 44.5 MB (26.90%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,18 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        for(int[] a: dp){
-            Arrays.fill(a,-1);
-        }
-        return path(0,0,triangle,dp);
+        return tabulation(triangle);
     }
-    public int path(int i,int j,List<List<Integer>> triangle,int[][] dp){
-        if(i==triangle.size()-1) return triangle.get(i).get(j);
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        int bottom = triangle.get(i).get(j) + path(i+1,j,triangle,dp);
-        int diagonal = triangle.get(i).get(j) + path(i+1,j+1,triangle,dp);
-        
-        return dp[i][j] = Math.min(bottom,diagonal);
+    public int tabulation(List<List<Integer>> triangle){
+        int[][] dp = new int[triangle.size()][triangle.size()];
+        for(int j=0;j<triangle.get(triangle.size()-1).size();j++){
+            dp[triangle.size()-1][j] = triangle.get(triangle.size()-1).get(j);
+        }
+        for(int i=dp.length-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                
+                dp[i][j] = Math.min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
