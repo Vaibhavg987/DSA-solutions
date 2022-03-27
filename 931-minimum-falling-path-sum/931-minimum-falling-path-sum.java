11f00113272a63f28ce971bf62fb5381ae240ac1
Vaibhav <78644114+Vaibhavg987@users.noreply.github.com>
2022-03-27 12:52:24 +0530

    Time: 7 ms (34.94%), Space: 47.9 MB (33.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,23 +1,35 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int i=matrix.length;
-        int j=matrix[0].length;
-        int[][] dp = new int[i][j];
-        int min = (int)1e9;
-        for(int col=0;col<j;col++){
-            min = Math.min(min,minPath(i-1,col,matrix,dp));
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int j=0;j<n;j++) dp[0][j] = matrix[0][j];
+        for(int i=1;i<m;i++){
+           for(int j=0;j<n;j++){
+                int s = matrix[i][j] + dp[i-1][j];
+               int l = matrix[i][j];
+               if(j-1>=0) l+=dp[i-1][j-1];
+               else l+=(int)1e8;
+               
+               int r = matrix[i][j];
+               if(j+1<n) r+= dp[i-1][j+1];
+                 else r+=(int)1e8;
+               
+               dp[i][j] = Math.min(s,Math.min(l,r));
+           }
+        }
+        int min = (int)1e8;
+        for(int col=0;col<n;col++){
+            min = Math.min(min,dp[m-1][col]);
         }
         return min;
     }
-    public int minPath(int i,int j,int[][] a,int[][] dp){
-        if(j<0||j>=a[0].length) return (int)1e9;
-        if(i==0) return a[0][j];
+//     public int minPath(int i,int j,int[][] a,int[][] dp){
+//         if(j<0||j>=a[0].length) return (int)1e9;
+//         if(i==0) return a[0][j];
         
-        if(dp[i][j] != 0) return dp[i][j];
-        int s = a[i][j] + minPath(i-1,j,a,dp);
-        int l = a[i][j] + minPath(i-1,j-1,a,dp);
-        int r = a[i][j] + minPath(i-1,j+1,a,dp);
-        
-        return dp[i][j] = Math.min(s,Math.min(l,r));
-    }
+//         if(dp[i][j] != 0) return dp[i][j];
+       
+//         return dp[i][j] = Math.min(s,Math.min(l,r));
+//     }
 }
